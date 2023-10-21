72c9a8b1948ff9e88062d80ac519cb2f15354306
nymulislam <naymulislam241@gmail.com>
2023-10-21 17:58:01 +0600

    Setup car universe server site

---
+++ index.js
@@ -0,0 +1,63 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.tloczwa.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const carsCollection = client.db("carsDB").collection("cars");
+
+        // create data
+        app.post('/cars', async (req, res) => {
+            const newCars = req.body;
+            console.log(newCars)
+            const result = await carsCollection.insertOne(newCars);
+            res.send(result)
+        })
+
+        // read data
+        app.get('/cars', async (req, res) => {
+            const cursor = carsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("Car Universe Server is Running...")
+})
+
+app.listen(port, () => {
+    console.log(`Car Universe Server is Running on ${port}`);
+})
